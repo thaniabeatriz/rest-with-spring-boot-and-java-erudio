6390ca517474672b70e5c83d65f76b4b3e91e850
Thania Beatriz <thaniabeatriz2017@gmail.com>
2025-06-18 21:10:27 -0400

    primeiros passas com spring boot

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/thania/model/Greeting.java
@@ -0,0 +1,4 @@
+package br.com.thania.model;
+
+public record Greeting(Long id, String content) {
+}
---
+++ rest-with-spring-boot-and-java-erudio/src/test/java/br/com/thania/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.thania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
